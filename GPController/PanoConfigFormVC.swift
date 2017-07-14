663cd2ce89a0764b5df15e73226c220a128f378e
David Fang <i.davidfang@gmail.com>
2017-07-14 00:54:55 -0700

    Add update functionality to stepper values and switch states

--- GPController/PanoConfigFormVC.swift
+++ GPController/PanoConfigFormVC.swift
@@ -15,10 +15,11 @@ class PanoConfigFormVC: UIViewController {
     @IBOutlet weak var fovStepper: GMStepper!
     @IBOutlet weak var overlapStepper: GMStepper!
     
-    let hFOV: Double = 70
-    let hOverlap: Double = 60
+    @IBOutlet weak var componentsToggle: UISwitch!
+    @IBOutlet weak var fovToggle: UISwitch!
+    @IBOutlet weak var overlapToggle: UISwitch!
+    
     let hLensFOV: Double = 90
-    let vFOV: Double = 30
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,5 +32,39 @@ class PanoConfigFormVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    fileprivate func toggleSwitch(_switch: UISwitch, on: Bool) {
+        _switch.setOn(on, animated: true)
+        _switch.isEnabled = on
+    }
+
+    @IBAction func updateValues(sender: GMStepper) {
+        if (componentsToggle.isOn && fovToggle.isOn) {
+            overlapStepper.value = GPCalculate.overlap(numComponents: componentsStepper.value, panoFOV: fovStepper.value, lensFOV: hLensFOV)
+        } else if (componentsToggle.isOn && overlapToggle.isOn) {
+            fovStepper.value = GPCalculate.panoFOV(numComponents: componentsStepper.value, lensFOV: hLensFOV, overlap: overlapStepper.value)
+        } else if (fovToggle.isOn && overlapToggle.isOn) {
+            componentsStepper.value = GPCalculate.numComponents(panoFOV: fovStepper.value, lensFOV: hLensFOV, overlap: overlapStepper.value)
+        }
+    }
+    
+    @IBAction func updateSwitch(sender: UISwitch) {
+        
+        toggleSwitch(_switch: sender, on: false)
+        
+        // New value should be !sender.isOn
+        // If turning off, find the one that is currently off
+        // and is not sender, and turn it one
+        
+        if (!componentsToggle.isOn && sender != componentsToggle) {
+            toggleSwitch(_switch: componentsToggle, on: true)
+            componentsToggle.isEnabled = true
+        } else if (!fovToggle.isOn && sender != fovToggle) {
+            toggleSwitch(_switch: fovToggle, on: true)
+        } else if (!overlapToggle.isOn && sender != overlapToggle) {
+            toggleSwitch(_switch: overlapToggle, on: true)
+        }
+    }
+    
 }
 
